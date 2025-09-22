becf2b02a8a6c4cb7587916bed18082f42990596
sankalpaacharya <sankalpaacharya01@gmail.com>
2025-09-22 21:21:37 +0530

    feat: collapsible sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   LucideIcon,
   PanelRightOpen,
@@ -10,6 +11,8 @@ import {
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
 import ChatThread from "./chat-thread";
+import { createContext, useState, useContext } from "react";
+import { cn } from "@/lib/utils";
 
 type SideBarLinkItem = {
   label: string;
@@ -51,68 +54,109 @@ const SideBarLinks: SideBarProps[] = [
   },
 ];
 
+type SideBarContextType = {
+  isOpen: boolean;
+};
+const SideBarContext = createContext<SideBarContextType | null>(null);
+
+function useSideBarContext() {
+  const context = useContext(SideBarContext);
+  if (!context) {
+    throw new Error("useSourceContext must be used within a Source component");
+  }
+  return context;
+}
+
 export default function Sidebar() {
+  const [isOpen, setIsOpen] = useState(false);
+  console.log(isOpen, "this is being callllled");
+
   return (
-    <div className="p-4 flex flex-col h-full border-r w-80">
-      {/* Header */}
-      <div className="flex justify-between items-center mb-4">
-        <span className="font-semibold tracking-tight text-sm text-muted-foreground">
-          Chatcn
-        </span>
-        <button className="text-muted-foreground hover:bg-muted p-2 rounded-lg transition-colors cursor-e-resize">
-          <PanelRightOpen size={20} />
-        </button>
-      </div>
-
-      {/* Search */}
-      <div className="relative mb-4">
-        <Search
-          size={20}
-          className="absolute left-3 top-1/2 -translate-y-1/2 text-muted-foreground"
-        />
-        <Input
-          placeholder="Search"
-          className="shadow-none outline-none rounded-full h-12 pl-10 text-sm w-full"
-        />
-      </div>
-
-      {/* Links & Headings */}
-      <div className="flex-1 overflow-y-auto">
-        <div className="space-y-1">
-          {SideBarLinks.map((item, i) => {
-            if ("href" in item && "Icon" in item) {
-              return <SideBarLink key={i} {...item} />;
-            }
-
-            if (item.type === "HEADING") {
-              return (
-                <div
-                  key={i}
-                  className="px-3 pt-4 pb-1 text-xs font-semibold text-muted-foreground uppercase tracking-wide"
-                >
-                  {item.label}
-                </div>
-              );
-            }
-          })}
+    <SideBarContext.Provider value={{ isOpen }}>
+      <div
+        className={cn(
+          "p-4 flex flex-col h-full border-r w-80 transition-all justify-center",
+          isOpen ? "w-80" : "w-20"
+        )}
+      >
+        {/* Header */}
+        <div className="flex justify-between items-center mb-4">
+          <span
+            className={cn(
+              "font-semibold tracking-tight text-sm text-muted-foreground",
+              !isOpen ? "hidden" : ""
+            )}
+          >
+            Chatcn
+          </span>
+          <button
+            onClick={() => setIsOpen((prev) => !prev)}
+            className="text-muted-foreground hover:bg-muted p-3 rounded-lg transition-colors cursor-e-resize"
+          >
+            <PanelRightOpen size={20} />
+          </button>
         </div>
 
-        <div className="mt-2">
-          <ChatThread />
+        <div className={cn("relative mb-4", !isOpen ? "hidden" : "")}>
+          <Search
+            size={20}
+            className="absolute left-3 top-1/2 -translate-y-1/2 text-muted-foreground"
+          />
+          <Input
+            placeholder="Search"
+            className="shadow-none outline-none rounded-full h-12 pl-10 text-sm w-full"
+          />
+        </div>
+
+        {/* Links & Headings */}
+        <div className="flex-1 overflow-y-auto">
+          <div className="space-y-1">
+            {SideBarLinks.map((item, i) => {
+              if ("href" in item && "Icon" in item) {
+                return <SideBarLink key={i} {...item} />;
+              }
+
+              if (item.type === "HEADING") {
+                return (
+                  <div
+                    key={i}
+                    className={cn(
+                      "px-3 pt-4 pb-1 text-xs font-semibold text-muted-foreground uppercase tracking-wide",
+                      !isOpen ? "hidden" : ""
+                    )}
+                  >
+                    {item.label}
+                  </div>
+                );
+              }
+            })}
+          </div>
+
+          <div className={cn("mt-2", !isOpen ? "hidden" : "")}>
+            <ChatThread />
+          </div>
         </div>
       </div>
-    </div>
+    </SideBarContext.Provider>
   );
 }
 
 function SideBarLink({ label, href, Icon }: SideBarLinkItem) {
+  const { isOpen } = useSideBarContext();
   return (
     <Link
       href={href}
       className="hover:bg-muted/70 flex gap-3 items-center px-3 py-3 rounded-lg transition-colors"
     >
       <Icon size={20} className="text-muted-foreground" />
-      <p className="text-sm font-medium tracking-wide">{label}</p>
+      <p
+        className={cn(
+          "text-sm font-medium tracking-wide",
+          !isOpen ? "hidden" : ""
+        )}
+      >
+        {label}
+      </p>
     </Link>
   );
 }
